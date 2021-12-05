f3c67ce77fae778be832590a46f27a57669bf7c7
Sindre <sindrepm@gmail.com>
2021-12-05 10:12:50 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <VApp>
+    <VMain>
+      <Nuxt />
+    </VMain>
+  </VApp>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <VContainer>
+    <VRow>
+      <VCol>
+        <h2>Simple Input</h2>
+        <CurrencyInput v-model="value" currency="EUR" />
+      </VCol>
+      <VCol>
+        <h2>Vuetify</h2>
+        <VCurrencyField v-model="value" :options="{ currency: 'EUR' }" />
+      </VCol>
+    </VRow>
+    <VRow>
+      <VCol
+        >Number value: <code>{{ value != null ? value : "null" }}</code></VCol
+      >
+    </VRow>
+  </VContainer>
+</template>
+
+<script>
+import { ref } from "@vue/composition-api";
+import VCurrencyField from "@/components/VCurrencyField";
+import CurrencyInput from "@/components/CurrencyInput";
+
+export default {
+  name: "App",
+  components: {
+    CurrencyInput,
+    VCurrencyField,
+  },
+  setup() {
+    return {
+      value: ref(1234),
+    };
+  },
+};
+</script>
+
